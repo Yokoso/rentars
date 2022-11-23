11c7e52217da02b4ffcb89bb334f182db540ee36
Yokoso <tomsolga90@gmail.com>
2022-11-23 23:15:49 +0100

    menu typo and slide bug fix

--- src/app/components/navbar/navItems.tsx
+++ src/app/components/navbar/navItems.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import styled from 'styled-components';
 import tw from 'twin.macro';
-import { Slide as Menu } from 'react-burger-menu';
+import { slide as Menu } from 'react-burger-menu';
 import { useMediaQuery } from 'react-responsive';
 import { SCREENS } from '../responsive';
 import menuStyles from './menuStyles';
@@ -34,7 +34,7 @@ export function NavItems() {
 
     if (isMobile) {
         return (
-            <Menu styles={ menuStyles }>
+            <Menu right styles={ menuStyles }>
                 <ListContainer>
                     <NavItem>
                         <a href="#">Home</a>
