dbf367b77ec8d5722ac3aad0e98057c037dfdab7
Yokoso <tomsolga90@gmail.com>
2022-11-24 10:20:43 +0100

    menu styles adjust

--- src/app/components/navbar/navItems.tsx
+++ src/app/components/navbar/navItems.tsx
@@ -1,5 +1,6 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
 import React from 'react';
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 import tw from 'twin.macro';
 import { slide as Menu } from 'react-burger-menu';
 import { useMediaQuery } from 'react-responsive';
@@ -13,7 +14,7 @@ const ListContainer = styled.ul`
     `};
 `;
 
-const NavItem = styled.li`
+const NavItem = styled.li<{ menu?: any }>`
     ${tw`
         text-xs
         md:text-base
@@ -27,6 +28,15 @@ const NavItem = styled.li`
         ease-in-out
         hover:text-gray-700
     `};
+
+    ${({ menu }) => menu && css`
+        ${tw`
+            text-white
+            text-xl
+            mb-3
+            focus:text-white
+        `};
+    `};
 `;
 
 export function NavItems() {
@@ -36,16 +46,16 @@ export function NavItems() {
         return (
             <Menu right styles={ menuStyles }>
                 <ListContainer>
-                    <NavItem>
+                    <NavItem menu>
                         <a href="#">Home</a>
                     </NavItem>
-                    <NavItem>
+                    <NavItem menu>
                         <a href="#">Cars</a>
                     </NavItem>
-                    <NavItem>
+                    <NavItem menu>
                         <a href="#">Services</a>
                     </NavItem>
-                    <NavItem>
+                    <NavItem menu>
                         <a href="#">Contact Us</a>
                     </NavItem>
                 </ListContainer>
