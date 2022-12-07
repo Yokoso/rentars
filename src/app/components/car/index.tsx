539b6dfc6dbad808503c91e909f4b78ff1b0d0e8
Yokoso <tomsolga90@gmail.com>
2022-12-07 18:11:35 +0100

    car interface implementation

---
+++ src/app/components/car/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { ICar } from '../../../typings/car';
+
+interface ICarProps extends ICar {
+    availability: boolean;
+}
+
+export function Car(props: ICarProps) {
+
+}
