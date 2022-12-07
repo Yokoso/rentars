addcac6e11eca95a5d02211a2094a7e474879e01
Yokoso <tomsolga90@gmail.com>
2022-12-07 17:54:58 +0100

    featuredCars render

---
+++ src/app/containers/HomePage/featuredCars.tsx
@@ -0,0 +1,50 @@
+import React from 'react';
+import styled from 'styled-components';
+import tw from 'twin.macro';
+
+const FeaturedCarsContainer = styled.div`
+    ${tw`
+        max-w-screen-lg
+        w-full
+        flex
+        flex-col
+        items-center
+        justify-center
+        pr-4
+        pl-4
+        md:pr-0
+        md:pl-0
+        mb-10
+    `};
+`;
+
+const Title = styled.h2`
+    ${tw`
+        text-3xl
+        lg:text-5xl
+        text-black
+        font-extrabold
+    `};
+`;
+
+const CarsContainer = styled.div`
+    ${tw`
+        w-full
+        flex
+        flex-wrap
+        justify-center
+        mt-7
+        md:mt-10
+    `};
+`;
+
+export function FeaturedCars() {
+    return (
+        <FeaturedCarsContainer>
+            <Title>Explore Our Top Deals</Title>
+            <CarsContainer>
+                
+            </CarsContainer>
+        </FeaturedCarsContainer>
+    );
+}
