eaff7027a04295211ddb352608a14a1d8d832b02
Yokoso <tomsolga90@gmail.com>
2022-12-10 14:08:13 +0100

    featuredCars button position fix

--- src/app/components/car/index.tsx
+++ src/app/components/car/index.tsx
@@ -12,8 +12,8 @@ interface ICarProps extends ICar {
 
 const CarContainer = styled.div`
     width: 16.5em;
-    min-height: 22.2em;
-    max-height: 22.2em;
+    min-height: 23em;
+    max-height: 23em;
     box-shadow: 0 1.3px 17px -2px rgba(0, 0, 0, 0.4);
 
     ${tw`
@@ -67,6 +67,7 @@ const SmallText = styled.p`
     color: inherit;
 
     ${tw`
+        inline-flex
         text-xs
         font-thin
     `};
