380aa8d9dbd7b8b428f7ed7dbac70a17c6ee9592
Yokoso <tomsolga90@gmail.com>
2022-12-16 00:33:49 +0100

    footer containers styles n additional sections add

--- src/app/components/footer/index.tsx
+++ src/app/components/footer/index.tsx
@@ -1,3 +1,5 @@
+import { faEnvelope, faPhoneAlt } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React from 'react';
 import styled from 'styled-components';
 import tw from 'twin.macro';
@@ -90,6 +92,35 @@ const HeaderTitle = styled.h3`
     `};
 `;
 
+const HorizontalContainer = styled.div`
+    ${tw`
+        flex
+        items-center
+    `};
+`;
+
+const RedIcon = styled.span`
+    ${tw`
+        w-9
+        h-9
+        rounded-full
+        bg-red-500
+        flex
+        items-center
+        justify-center
+        text-white
+        text-base
+        mr-2
+    `};
+`;
+
+const SmallText = styled.h6`
+    ${tw`
+        text-white
+        text-sm
+    `};
+`;
+
 export function Footer() {
     return (
         <FooterContainer>
@@ -121,6 +152,26 @@ export function Footer() {
                         <ListItem><a href="#">Privacy Policy</a></ListItem>
                         <ListItem><a href="#">Terms &amp; conditions</a></ListItem>
                     </LinksList>
+                </SectionContainer>
+
+                <SectionContainer>
+                    <HeaderTitle>Call now</HeaderTitle>
+                    <HorizontalContainer>
+                        <RedIcon>
+                            <FontAwesomeIcon icon={faPhoneAlt} />
+                        </RedIcon>
+                        <SmallText>+48 876 362 746</SmallText>
+                    </HorizontalContainer>
+                </SectionContainer>
+
+                <SectionContainer>
+                    <HeaderTitle>Mail</HeaderTitle>
+                    <HorizontalContainer>
+                        <RedIcon>
+                            <FontAwesomeIcon icon={faEnvelope} />
+                        </RedIcon>
+                        <SmallText>info@rentars.com</SmallText>
+                    </HorizontalContainer>
                 </SectionContainer>
             </InnerContainer>
         </FooterContainer>
